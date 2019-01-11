15e525731f08596cbe996d7462ac31701d598e02
elhassler <elhassler@edu.aau.at>
2019-01-11 16:00:46 +0100

    login form

--- AngularVierGewinnt/src/app/app-routing.module.ts
+++ AngularVierGewinnt/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import {RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'LoginComponent', component: LoginComponent},
+  { path: 'RegistrationComponent', component: RegistrationComponent},
+  { path: '', redirectTo: 'LoginComponent', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- AngularVierGewinnt/src/app/app.module.ts
+++ AngularVierGewinnt/src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { GamescreenComponent } from './gamescreen/gamescreen.component';
 import { MatchmakingComponent } from './matchmaking/matchmaking.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+
+const appRoutes: Routes = [
+  { path: 'LoginComponent', component: LoginComponent},
+  { path: 'RegistrationComponent', component: RegistrationComponent},
+  { path: '', redirectTo: 'LoginComponent', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     GamescreenComponent,
-    MatchmakingComponent
+    MatchmakingComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
